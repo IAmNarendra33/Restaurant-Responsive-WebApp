85fe80501793e0eab3b4d0c0551718d1d8f18394
IAmNarendra33 <148229698+IAmNarendra33@users.noreply.github.com>
2023-10-20 23:17:07 -0700

    first commit

---
+++ src/component/basic/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "../image/meggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "../image/aalupkora.jpeg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        " Delicious aalupkoda, our specialty, is a crispy, flavorful potato fritter, a perfect blend of spices, and a must-try at our restaurant Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "image/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../image/chole.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Delicious homemade chole – a flavorful North Indian dish with chickpeas, spices, and a tangy tomato gravy, served at our restaurant",
+    },
+    {
+      id: 5,
+      image: "../image/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Discover an exquisite culinary experience at our restaurant, where we elevate corn to a gourmet delight. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../image/nonvegthali.png",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus  . ",
+    },
+    {
+      id: 7,
+      image: "../image/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Indulge in our restaurant's delectable sweets, where every bite is a symphony of flavors and a journey of culinary delight",
+    },
+    {
+      id: 8,
+      image: "../image/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+      "Rajma Chaval: A culinary delight featuring creamy kidney beans cooked in aromatic spices, served with fluffy basmati rice. Our restaurant's signature dish offers a taste of North Indian comfort in every bite.",
+    },
+    {
+      id: 9,
+      image: "../image/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Delight your taste buds with our restaurant's exquisite samosas - Enjoy the rich, authentic taste of India right at our table – a true culinary adventure in just one bite",
+    },
+  ];
+  
+  export default Menu;
